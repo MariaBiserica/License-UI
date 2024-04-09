86eee57757858ca63b7921a48bff93adae782b12
Maria Biserica <maria.biserica@student.unitbv.ro>
2024-04-10 02:49:42 +0300

    Added Firebase confirguration.

--- license_peaksight/lib/main.dart
+++ license_peaksight/lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:license_peaksight/constants.dart';
+import 'package:license_peaksight/firebase_options.dart';
 import 'package:license_peaksight/widget_tree.dart';
 
-void main() {
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
