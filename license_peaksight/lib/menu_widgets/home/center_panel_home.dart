3557d5f3e75d0a3cc05bbaedfd3344036fe1eb40
Maria Biserica <maria.biserica@student.unitbv.ro>
2024-04-30 02:26:25 +0300

    Made center panel update in real time.

--- license_peaksight/lib/menu_widgets/home/center_panel_home.dart
+++ license_peaksight/lib/menu_widgets/home/center_panel_home.dart
@@ -13,37 +13,23 @@ class CenterPanelHome extends StatefulWidget {
 
 class _CenterPanelHomeState extends State<CenterPanelHome> {
   final AuthService _authService = AuthService();
-  List<ChartData> chartsData = [];
 
-  @override
-  void initState() {
-    super.initState();
-    fetchTasks();
-  }
-
-  void fetchTasks() async {
+  Stream<List<ChartData>> taskStream() async* {
     String? userId = _authService.getCurrentUserId();
     if (userId != null) {
-      var snapshot = await FirebaseFirestore.instance
+      yield* FirebaseFirestore.instance
           .collection('tasks')
           .where('userId', isEqualTo: userId)
-          .get();
-
-      Map<String, Map<String, int>> categoryData = {};
-
-      snapshot.docs.forEach((doc) {
-        Task task = Task.fromMap(doc.data() as Map<String, dynamic>, doc.id);
-        categoryData[task.category] = categoryData[task.category] ?? {};
-        categoryData[task.category]?[task.status] = (categoryData[task.category]?[task.status] ?? 0) + 1;
-      });
-
-      List<ChartData> tempData = categoryData.entries.map((entry) {
-        return ChartData(entry.key, entry.value);
-      }).toList();
-
-      setState(() {
-        chartsData = tempData;
-      });
+          .snapshots()
+          .map((snapshot) {
+            Map<String, Map<String, int>> categoryData = {};
+            for (var doc in snapshot.docs) {
+              Task task = Task.fromMap(doc.data() as Map<String, dynamic>, doc.id);
+              categoryData[task.category] = categoryData[task.category] ?? {};
+              categoryData[task.category]?[task.status] = (categoryData[task.category]?[task.status] ?? 0) + 1;
+            }
+            return categoryData.entries.map((entry) => ChartData(entry.key, entry.value)).toList();
+          });
     }
   }
 
@@ -64,7 +50,7 @@ class _CenterPanelHomeState extends State<CenterPanelHome> {
           ),
         ],
       ),
-      child: SingleChildScrollView( // To ensure the content is scrollable if it overflows
+      child: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
@@ -75,55 +61,33 @@ class _CenterPanelHomeState extends State<CenterPanelHome> {
                 fontFamily: 'MOXABestine', 
                 fontSize: 20, 
                 fontWeight: FontWeight.bold, 
-                color: Colors.white
-                ),
-              ),
-            _buildStatisticsWidgets(),
-            //_buildRecentTasksWidget(),
+                color: Colors.white),
+            ),
+            StreamBuilder<List<ChartData>>(
+              stream: taskStream(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    children: [
+                      Text(
+                        'Statistics',
+                        style: TextStyle(
+                          fontFamily: 'MOXABestine',
+                          fontSize: 20, 
+                          fontWeight: FontWeight.bold, 
+                          color: Colors.white),
+                      ),
+                      PieChartGoals(snapshot.data!),
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return Text("Error: ${snapshot.error}");
+                }
+                return CircularProgressIndicator();
+              },
+            ),
           ],
         ),
-      ),
-    );
-  }
-
-  Widget _buildStatisticsWidgets() {
-    return Column(
-      children: [
-        Text(
-          'Statistics',
-          style: TextStyle(
-            fontFamily: 'MOXABestine',
-            fontSize: 20, 
-            fontWeight: FontWeight.bold, 
-            color: Colors.white),
-        ),
-        if (chartsData.isNotEmpty) 
-          PieChartGoals(chartsData),
-      ],
-    );
-  }
-
-  Widget _buildRecentTasksWidget() {
-    // Placeholder for recent tasks widget
-    return Expanded(
-      child: ListView.builder(
-        itemCount: 5, // Example count of tasks
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text("Task $index", style: TextStyle(color: Colors.white)),
-            subtitle: Text("Status: Completed", style: TextStyle(color: Colors.green)),
-            onTap: () {
-              // Example of action on tap
-              showDialog(
-                context: context,
-                builder: (_) => AlertDialog(
-                  title: Text('Task Details'),
-                  content: Text('Details of Task $index here.'),
-                ),
-              );
-            },
-          );
-        },
       ),
     );
   }
